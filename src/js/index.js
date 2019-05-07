60ae443166bc767afdb881bcc9e5007d0811307e
Ruslan Igorevich <ruslan.igorevich@list.ru>
2019-05-07 12:47:33 +0300

    add core-js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,22 @@
 import divide from "./lib.js";
 import $ from "jquery";
 
+import "core-js/features/promise";
+
 console.log(divide(4, 2));
 console.log(divide(5, 2));
 console.log(divide(15, 2));
 console.log(divide(105, 2));
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     console.log(1);
     $(".page-title").addClass("color-black");
 
+});
+
+(new Promise(function (resolve, reject) {
+    setTimeout(resolve, 500);
+})).then(() => {
+    console.log("promise resolved");
 });
